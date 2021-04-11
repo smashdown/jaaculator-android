d1136f0f54b7d8fd2ed79b77d62187bb8429dc16
Jongyoung Jeong <jy@appsynth.net>
2021-04-11 15:39:55 +0700

    fix build

--- app/src/main/java/com/hechikasoft/jaaculator/JaaculatorApp.kt
+++ app/src/main/java/com/hechikasoft/jaaculator/JaaculatorApp.kt
@@ -1,6 +1,7 @@
 package com.hechikasoft.jaaculator
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class JaaculatorApp: Application() {
--- app/src/main/java/com/hechikasoft/jaaculator/presentation/memberlist/MemberListActivity.kt
+++ app/src/main/java/com/hechikasoft/jaaculator/presentation/memberlist/MemberListActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.hechikasoft.jaaculator.presentation.memberlist.ui.theme.JaaculatorTheme
+import com.hechikasoft.jaaculator.ui.theme.JaaculatorTheme
 
 class MemberListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
